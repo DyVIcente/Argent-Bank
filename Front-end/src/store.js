1456a8561886e1f1500a20722c3e43588fca6028
Dylan Vicente <dvicente@fidme.com>
2024-03-01 11:12:13 +0100

    feat: last push

--- Front-end/src/store.js
+++ Front-end/src/store.js
@@ -5,9 +5,7 @@ import userReducer from './reducers/user';
 const store = configureStore({
   reducer: {
     user: userReducer,
-    // Ajoutez d'autres reducers si nécessaire
   },
-  // Ajoutez d'autres options si nécessaire
 });
 
 export default store;
